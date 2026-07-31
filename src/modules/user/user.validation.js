3238aa3a59dc25384825e8b4f2985a41aeff3d5c
Nahida Akter Jenifa <jefrahman32@gmail.com>
2026-07-31 23:50:02 +0600

    Add user model and request validation

---
+++ src/modules/user/user.validation.js
@@ -0,0 +1,36 @@
+import { body, validationResult } from "express-validator";
+
+export const createUserValidation = [
+  body("name")
+    .trim()
+    .notEmpty()
+    .withMessage("Name is required")
+    .isLength({ min: 2, max: 50 })
+    .withMessage("Name must be between 2 and 50 characters"),
+
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid email"),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
+];
+
+export const validate = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      errors: errors.array(),
+    });
+  }
+
+  next();
+};
