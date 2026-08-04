4f5531ae8e6315e3f48c3ec5e72ac07689c89f3c
Nahida Akter Jenifa <jefrahman32@gmail.com>
2026-08-04 22:43:51 +0600

    feat(user): add get all, get by id, get me, update, and delete user APIs

--- src/app/modules/user/user.router.js
+++ src/app/modules/user/user.router.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
 import { userController } from "./user.controller.js";
+import { Role } from "./user.model.js";
+import { validateRequest } from "../../middlewares/validateRequest.js";
+import { updateUserValidation } from "./user.validation.js";
 
 const userRouter = Router();
 
 userRouter.post("/create", userController.createUser);
-userRouter.get("/all", userController.getUsers);
-userRouter.get("/:id", userController.getUser);
-userRouter.patch("/:id", userController.updateUser);
+userRouter.get("/all-users", userController.getUsers);
+userRouter.get("/me", checkAuth(...Object.values(Role)), userController.getMe);
+userRouter.get("/:id",  checkAuth(Role.ADMIN, Role.SUPER_ADMIN), userController.getUser);
+userRouter.patch(
+  "/:id",
+  validateRequest(updateUserValidation),
+  checkAuth(...Object.values(Role)),
+  userController.updateUser,
+)
 userRouter.delete("/:id", userController.deleteUser);
 
 export default userRouter;
--- src/app/modules/user/user.service.js
+++ src/app/modules/user/user.service.js
@@ -1,6 +1,8 @@
 import bcryptjs from "bcryptjs";
-import User from "./user.model.js";
+import User, { Role } from "./user.model.js";
 import httpStatus from "http-status-codes";
+import { QueryBuilder } from "../../utils/QueryBuilder.js";
+import { userSearchableFields } from "./user.constant.js";
 
 const createUser = async (payload) => {
   const { email, password, ...rest } = payload;
@@ -25,27 +27,132 @@ const createUser = async (payload) => {
   return user;
 };
 
-const getUsers = async () => {
-  return await User.find();
+const getUsers = async (query) => {
+  const queryBuilder = new QueryBuilder(User.find(), query);
+
+  const usersData = queryBuilder
+    .filter()
+    .search(userSearchableFields)
+    .sort()
+    .fields()
+    .paginate();
+
+  const [data, meta] = await Promise.all([
+    usersData.build(),
+    queryBuilder.getMeta(),
+  ]);
+
+  return {
+    data,
+    meta,
+  };
+};
+
+const getMe = async (userId) => {
+  const user = await User.findById(userId).select("-password");
+
+  return {
+    data: user,
+  };
 };
 
 const getUser = async (id) => {
-  return await User.findById(id);
+  const user = await User.findById(id).select("-password");
+
+  return {
+    data: user,
+  };
 };
 
-const updateUser = async (id, data) => {
-  return await User.findByIdAndUpdate(id, data, {
-    new: true,
-  });
+const updateUser = async (userId, payload, decodedToken) => {
+  if (
+    decodedToken.role === Role.USER 
+  ) {
+    if (userId !== decodedToken.userId) {
+      throw new AppError(401, "You are not authorized");
+    }
+  }
+
+  const ifUserExist = await User.findById(userId);
+
+  if (!ifUserExist) {
+    throw new AppError(httpStatus.NOT_FOUND, "User Not Found");
+  }
+
+  if (
+    decodedToken.role === Role.ADMIN &&
+    ifUserExist.role === Role.SUPER_ADMIN
+  ) {
+    throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+  }
+
+  if (payload.role) {
+    if (
+      decodedToken.role === Role.USER
+    ) {
+      throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+    }
+
+
+  }
+
+  if (
+    payload.isActive ||
+    payload.isDeleted ||
+    payload.isVerified
+  ) {
+    if (
+      decodedToken.role === Role.USER
+    ) {
+      throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+    }
+  }
+
+  const newUpdatedUser = await User.findByIdAndUpdate(
+    userId,
+    payload,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  return newUpdatedUser;
 };
 
-const deleteUser = async (id) => {
-  return await User.findByIdAndDelete(id);
+
+const deleteUser = async (userId, decodedToken) => {
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, "User not found");
+  }
+
+  if (
+    decodedToken.role === Role.ADMIN &&
+    user.role === Role.SUPER_ADMIN
+  ) {
+    throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+  }
+
+  const deletedUser = await User.findByIdAndUpdate(
+    userId,
+    { isDeleted: true },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  return deletedUser;
 };
+
+
 
 export const userServices = {
   createUser,
   getUsers,
+  getMe,
   getUser,
   updateUser,
   deleteUser,
